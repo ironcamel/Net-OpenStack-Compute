cdd21f79e68a0dedb850d64a5aaf2f427840e3f3
Naveed Massjouni <naveedm9@gmail.com>
2011-12-22 16:51:10 -0500

    Major oscompute CLI changes. Added Auth module.

--- lib/Net/OpenStack/Compute.pm
+++ lib/Net/OpenStack/Compute.pm
@@ -1,5 +1,5 @@
 package Net::OpenStack::Compute;
-use Moose;
+use Any::Moose;
 
 # VERSION
 
@@ -7,30 +7,60 @@ use Carp;
 use HTTP::Request;
 use JSON qw(to_json);
 use LWP;
+use Net::OpenStack::Compute::Auth;
 
-has token    => (is => 'ro');
-has base_url => (is => 'ro');
-has agent => (
+has auth_url   => (is => 'ro', isa => 'Str', required => 1);
+has user       => (is => 'ro', isa => 'Str', required => 1);
+has key        => (is => 'ro', isa => 'Str', required => 1);
+has project_id => (is => 'ro');
+has region     => (is => 'ro');
+
+has _auth => (
+    is   => 'rw',
+    isa  => 'Net::OpenStack::Compute::Auth',
+    lazy => 1,
+    default => sub {
+        my $self = shift;
+        Net::OpenStack::Compute::Auth->new(
+            auth_url   => $self->auth_url,
+            user       => $self->user,
+            password   => $self->key,
+            project_id => $self->project_id,
+            region     => $self->region,
+        );
+    },
+);
+
+has _base_url => (
+    is   => 'ro',
+    isa  => 'Str',
+    lazy => 1,
+    default => sub { shift->_auth->base_url },
+);
+
+has _agent => (
     is => 'ro',
     lazy => 1,
     default => sub {
         my $self = shift;
         my $agent = LWP::UserAgent->new();
-        $agent->default_header(x_auth_token => $self->token);
+        $agent->default_header(x_auth_token => $self->_auth->token);
         return $agent;
     },
 );
 
 sub get_servers {
-    my ($self) = @_;
-    my $base_url = $self->base_url;
-    return $self->agent->get("$base_url/servers/detail")->content;
+    my ($self, %params) = @_;
+    my $detail = '/detail';
+    $detail = '' if exists $params{detail} && !$params{detail};
+    my $base_url = $self->_base_url;
+    return $self->_agent->get("$base_url/servers$detail")->content;
 }
 
 sub get_server {
     my ($self, $id) = @_;
-    my $base_url = $self->base_url;
-    return $self->agent->get("$base_url/servers/$id")->content;
+    my $base_url = $self->_base_url;
+    return $self->_agent->get("$base_url/servers/$id")->content;
 }
 
 sub create_server {
@@ -39,9 +69,9 @@ sub create_server {
     croak "name param is required"   unless defined $name;
     croak "flavor param is required" unless defined $flavor;
     croak "image param is required"  unless defined $image;
-    my $base_url = $self->base_url;
+    my $base_url = $self->_base_url;
 
-    my $res = $self->agent->post(
+    my $res = $self->_agent->post(
         "$base_url/servers",
         content_type => 'application/json',
         Content => to_json({
@@ -57,10 +87,10 @@ sub create_server {
 
 sub delete_server {
     my ($self, $id) = @_;
-    my $base_url = $self->base_url;
+    my $base_url = $self->_base_url;
 
     my $req = HTTP::Request->new('DELETE', "$base_url/servers/$id");
-    my $res = $self->agent->request($req);
+    my $res = $self->_agent->request($req);
     return $res->is_success;
 }
 
@@ -70,20 +100,32 @@ sub delete_server {
 
     use Net::OpenStack::Compute;
     my $compute = Net::OpenStack::Compute->new(
-        base_url => 'http://...',
-        token    => 'secret',
+        auth_url   => $auth_url,
+        user       => $user,
+        key        => $password,
+        project_id => $project_id, # Optional
+        region     => $egion,      # Optional
     );
-    $compute->get_servers();
+    $compute->create_server(name => 's1', flavor => $flav_id, image => $img_id);
 
 =head1 METHODS
 
-=head2 get_server($id)
+=head2 get_server
 
-=head2 get_servers()
+    get_server($id)
 
-=head2 create_server(name => $name, flavor => $flavor, image => $image)
+=head2 get_servers
 
-=head2 delete_server($id)
+    get_servers()
+    get_servers(detail => 0)
+
+=head2 create_server
+
+    create_server(name => $name, flavor => $flavor, image => $image)
+
+=head2 delete_server
+
+    delete_server($id)
 
 =cut
 
---
+++ lib/Net/OpenStack/Compute/Auth.pm
@@ -0,0 +1,103 @@
+package Net::OpenStack::Compute::Auth;
+use Any::Moose;
+
+use JSON qw(from_json to_json);
+use LWP;
+
+has auth_url   => (is => 'rw', isa => 'Str', required => 1);
+has user       => (is => 'ro', isa => 'Str', required => 1);
+has password   => (is => 'ro', isa => 'Str', required => 1);
+has project_id => (is => 'ro');
+has region     => (is => 'ro');
+has _store     => (is => 'ro', lazy => 1, builder => '_build_store');
+
+has base_url => (
+    is => 'ro',
+    lazy => 1,
+    default => sub { shift->_store->{base_url} },
+);
+
+has token => (
+    is => 'ro',
+    lazy => 1,
+    default => sub { shift->_store->{token} },
+);
+
+sub BUILD {
+    my ($self) = @_;
+    # Make sure trailing slash is removed from auth_url
+    my $auth_url = $self->auth_url;
+    $auth_url =~ s/\/$//;
+    $self->auth_url($auth_url);
+}
+
+sub _build_store {
+    my ($self) = @_;
+    my $auth_url = $self->auth_url;
+    my ($version) = $auth_url =~ /(v\d\.\d)$/;
+    die "Could not determine version from url [$auth_url]" unless $version;
+    return $version eq 'v1.1' ? $self->auth_basic() : $self->auth_keystone();
+}
+
+sub auth_basic {
+    my ($self) = @_;
+    my $ua = LWP::UserAgent->new();
+    my $res = $ua->get($self->auth_url,
+        x_auth_user       => $self->user,
+        x_auth_key        => $self->password,
+        x_auth_project_id => $self->project_id,
+    );
+    #say $res->headers->as_string;
+    die $res->status_line . "\n" . $res->content unless $res->is_success;
+
+    return {
+        base_url   => $res->header('x-server-management-url'),
+        token => $res->header('x-auth-token'),
+    };
+}
+
+sub auth_keystone {
+    my ($self) = @_;
+    my $ua = LWP::UserAgent->new();
+    my $auth_data = {
+        auth =>  {
+            passwordCredentials => {
+                username => $self->user,
+                password => $self->password,
+            }
+        }
+    };
+
+    my $res = $ua->post($self->auth_url . "/tokens",
+        content_type => 'application/json', Content => to_json($auth_data));
+    
+    die $res->status_line . "\n" . $res->content unless $res->is_success;
+    my $data = from_json($res->content);
+    my $token = $data->{access}{token}{id};
+
+    my ($catalog) =
+        grep { $_->{type} eq 'compute' } @{$data->{access}{serviceCatalog}};
+    die "No compute service catalog found" unless $catalog;
+
+    my $base_url = $catalog->{endpoints}[0]{publicURL};
+    if ($self->region) {
+        for my $endpoint (@{ $catalog->{endpoints} }) {
+            if ($endpoint->{region} eq $self->region) {
+                $base_url = $endpoint->{publicURL};
+                last;
+            }
+        }
+    }
+    return { base_url => $base_url, token => $token };
+}
+
+# ABSTRACT: Steroids for your perl one-liners.
+
+=head1 DESCRIPTION
+
+This module contains helper methods used by stackattack.
+You probably want to look at the documentation for L<stackattack> instead.
+
+=cut
+
+1;
